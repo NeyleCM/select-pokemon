609f2a30fbadeb2ce22112902321acbb68881a11
NeyleCM <neylecmorantes@gmail.com>
2024-06-22 01:09:56 +0200

    Challenge completado pokemon

--- js/script.js
+++ js/script.js
@@ -4,45 +4,29 @@
 
 // 3. Cuando selecciones un Pokémon y hagas clic en el botón "Obtener Información", se tendrá que mostrar la información en pantalla del Pokémon, incluyendo su nombre, imagen, tipo, altura y peso.
 
-const bulbasaur = document.getElementById("bulbasaur");
-const charmander = document.getElementById("charmander");
-const squirtle = document.getElementById("squirtle");
-
-const pokemonSelect = document.getElementById("pokemon-select").value;
-console.log(pokemonSelect);
 
 const obtenerBoton = document.getElementById("get-pokemon");
 
 const getPokemon = () => {
-  if (Option.value === bulbasaur) {
-    return obtenerBoton;
-  }
-  if (Option.value === charmander) {
-    return obtenerBoton;
-  } else if (Option.value === squirtle) {
-    return obtenerBoton;
-  }
-};
-console.log(getPokemon);
-
-document
-  .getElementById("get-pokemon")
-  .pokemonSelect.addEventListener("click", () => {
+const pokemonSelect = document.getElementById("pokemon-select").value;
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonSelect}`)
       .then((response) => response.json())
-      .then((data) =>
-}
-    const pokemon = data.results
-      character.forEach(character => {
+      .then((data) => {
         const template = `
-              <h2><strong>Nombre:</strong> ${
-                data.name.toUpperCase() + data.name.slice(1)
-              }</h2>
-              <img src="${data.sprites.front_default}" alt="${data.name}">
+              <h2><strong>Nombre:</strong> ${data.name.toUpperCase()}</h2>
+              <img class="imagen" src="${data.sprites.front_default}" alt="${data.name}">
               <p><strong>Tipo:</strong> ${data.types
                 .map((type) => type.type.name)
-                .join(", ")}</p>
+                .join(" - ")}</p>
               <p><strong>Altura:</strong> ${data.height} m</p>
-              <p><strong>Peso:</strong> ${data.weight} kg</p>
-          `;
-  });
+              <p><strong>Peso:</strong> ${data.weight} kg</p>`;
+              
+obtenerBoton.innerHTML = template;
+
+        })  
+        .catch(error => {
+          console.error("Error al obtener los datos del Pokémon:", error);
+        });
+  }
+
+obtenerBoton.addEventListener("click", getPokemon);
